6420dca6b6a6a8978dce01e6f3ba1d1cbae437f6
Kowshik Diamond <68288912+kowshikdiamond@users.noreply.github.com>
2023-08-22 13:19:28 +0530

    Create github_api.sh

---
+++ github_api.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script helps to communicate and retrieve information from GitHub
+
+# Prompt user for Personal Access Token (PAT)
+read -p "Enter your Personal Access Token: " PAT
+
+# Prompt for API endpoint
+read -p "Enter the GitHub API endpoint you want to access: " API_ENDPOINT
+
+API="https://api.github.com/${API_ENDPOINT}"
+
+# Make the GitHub API call and display repository URLs
+response=$(curl -s -H "Authorization: token ${PAT}" ${API})
+
+# Check if the response is JSON and contains an array
+if jq -e '. | if type == "array" then true else false end' <<< "$response" >/dev/null; then
+    # Display array items as repository URLs
+    jq -r '.[] | .html_url' <<< "$response"
+else
+    # Display full response
+    echo "$response"
+fi
